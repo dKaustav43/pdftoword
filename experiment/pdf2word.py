0f7d085c8995b983cbab1153a5aeb86c4960b976
dKaustav43 <dkaustav43@gmail.com>
2026-03-19 13:18:20 +0530

    removed the docx feature for a .txt output

--- experiment/pdf2word.py
+++ experiment/pdf2word.py
@@ -1,8 +1,8 @@
 import pdfplumber
-from docx import Document
+#from docx import Document
 from pathlib import Path
 
-def pdftoword(input_pdf_path:str, output_word_path:str, start:int, end:int):
+def pdftoword(start:int, end:int, input_pdf_path:str = "data/pdfs/2025-HVM-Catapult-annual-report.pdf", output_text_path:str = "data/output_txts/exp2.txt"):
     
     '''
     The following code takes a pdf as an input and converts to a docx file.
@@ -21,28 +21,30 @@ def pdftoword(input_pdf_path:str, output_word_path:str, start:int, end:int):
         raise ValueError("Page range is out of bounds"
                          f"PDF has {total_pages} pages.")
     
-    doc = Document()
+    #doc = Document()
 
-    with pdfplumber.open(input_path) as pdf:
-        for i, page in enumerate(pdf.pages[start-1:end]):
-            text = page.extract_text()
-            if text:
-                doc.add_paragraph(f"--- Page {i+1} ---")
-                doc.add_paragraph(text)
+    with open(output_text_path, "w") as file:   
+        with pdfplumber.open(input_path) as pdf:
+            for i, page in enumerate(pdf.pages[start-1:end]):
+                text = page.extract_text(layout=True)
+                if text:
+                    # doc.add_paragraph(f"--- Page {i+1} ---")
+                    # doc.add_paragraph(text)
+                    file.write(text)
 
-    doc.save(output_word_path)
+    #doc.save(output_word_path)
 
-    return str(output_word_path)
+    return str(output_text_path)
 
 def main():
 
-    input_pdf_path = input("Enter the path of the pdf file here: ")
-    output_word_path = input("Enter the path where you would like to store the converted word file:")
+    # input_pdf_path = input("Enter the path of the pdf file here: ")
+    # output_word_path = input("Enter the path where you would like to store the converted word file:")
 
     start = int(input("Start page index: "))
     end = int(input("End page index:"))
 
-    result = pdftoword(input_pdf_path,output_word_path,start,end)
+    result = pdftoword(start,end)
 
     print(f"Created {result}")
 
